cf8bb88851ce0da8e1f525fe893c7c92de273570
vasudevchavan <30864214+vasudevchavan@users.noreply.github.com>
2022-01-10 20:27:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World git and terraform we are deploying a new application" > index.html
+              echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
@@ -51,22 +51,27 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
-resource "random_pet" "sg" {}
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
+}
+
+
+resource "random_pet" "sg1" {}
 
 resource "aws_instance" "web1" {
   ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web1-sg.id]
+  vpc_security_group_ids = [aws_security_group.web-sg1.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World git and terraform we are deploying a web application 2" > index.html
+              echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
 
-resource "aws_security_group" "web1-sg" {
-  name = "${random_pet.sg.id}-sg"
+resource "aws_security_group" "web-sg1" {
+  name = "${random_pet.sg1.id}-sg1"
   ingress {
     from_port   = 8080
     to_port     = 8080
@@ -75,10 +80,6 @@ resource "aws_security_group" "web1-sg" {
   }
 }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
-}
-
 output "web-address1" {
-  value = "${aws_instance.web1.public_dns}:8081"
+  value = "${aws_instance.web.public_dns}:8080"
 }
